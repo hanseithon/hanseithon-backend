5f564617d6158b38866a035edc9dd1b2b51491ee
kb4ek <kbaek1202@gmail.com>
2019-07-26 02:10:14 +0900

    유저 생성 API

--- routes/middlewares/user/register/register.js
+++ routes/middlewares/user/register/register.js
@@ -1,13 +1,21 @@
-const User = require('../../../../database/models/user.model');
+const User = require('../../../../database').User;
 
 const register = async (req, res, next) => {
   const user = res.locals.user;
+  const temp = res.locals.temp;
+
+  console.log(user);
 
   try {
     await User.create({
       id: user.id,
-      password: user.password,
+      password: temp.password,
       name: user.name,
+      salt: temp.salt,
+    });
+
+    res.json({
+      success: true,
     });
   } catch (err) {
     console.log(err);
