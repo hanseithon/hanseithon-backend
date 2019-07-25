dc83dc24bbd3ceb7961eb0eb61ff23c3d56ee127
kb4ek <kbaek1202@gmail.com>
2019-07-26 03:32:20 +0900

    boardController 라우팅

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/user', require('./routes/controllers/user.controller'));
+app.use('/board', require('./routes/controllers/board.controller'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
