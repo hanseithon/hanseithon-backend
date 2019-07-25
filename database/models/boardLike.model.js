7199296ab216705da672c61016e6a35715d2e7fd
kb4ek <kbaek1202@gmail.com>
2019-07-26 03:34:10 +0900

    charset: utf-8 추가

--- database/models/boardLike.model.js
+++ database/models/boardLike.model.js
@@ -1,21 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('boardLike', {
-    pk: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+  return sequelize.define(
+    'boardLike',
+    {
+      pk: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_pk: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      board_pk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    user_pk: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-    },
-    content: {
-      type: DataTypes.STRING(500),
-      allowNull: false,
-    },
-  });
+    {
+      charset: 'utf8',
+      collate: 'utf8_unicode_ci',
+    }
+  );
 };
--- database/models/user.model.js
+++ database/models/user.model.js
@@ -1,25 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('User', {
-    pk: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+  return sequelize.define(
+    'User',
+    {
+      pk: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      id: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      salt: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING(5),
+        allowNull: false,
+      },
     },
-    id: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    salt: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING(5),
-      allowNull: false,
-    },
-  });
+    {
+      charset: 'utf8',
+      collate: 'utf8_unicode_ci',
+    }
+  );
 };
