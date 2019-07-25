fd57a5671741974cb770d5131552f274ba75b6e1
kb4ek <kbaek1202@gmail.com>
2019-07-26 02:09:02 +0900

    userController 생성

--- routes/controllers/user.controller.js
+++ routes/controllers/user.controller.js
@@ -1,31 +1,27 @@
-/* //데이터 형식 체크
-const registerValidation = ;
-const checkValidation = ;
+//데이터 형식 검증
+/* const registerValidation = require('../middlewares/user/register/_validation'); */
 
 //유저 중복 체크
-const checkUser = 
+const checkUser = require('../middlewares/user/common/checkUser');
 
 //회원가입 시 비밀번호와 재입력 비밀번호 일치하는지 확인
-const checkPassword = ;
+const checkPassword1 = require('../middlewares/user/register/checkPassword');
+
+//로그인 시 비밀번호 확인
+const checkPassword2 = require('../middlewares/user/login/checkPassword2');
+
+//회원가입
+const register = require('../middlewares/user/register/register');
+
+//로그인
+const issueToken = require('../middlewares/jwt/issueToken');
 
 //비밀번호 암호화
-const passwordEncryption = ;
-
-const register =; */
+const passwordEncryption = require('../middlewares/user/common/passwordEncryption');
 
 const router = require('express').Router();
 
-console.log(3);
-router.post('/register', require('../middlewares/user/register/_validation'));
-console.log(4);
-router.use(require('../middlewares/common/checkValidation'));
-
-router.post(
-  '/register',
-  require('../middlewares/user/common/checkUser'),
-  require('../middlewares/user/register/checkPassword'),
-  require('../middlewares/user/common/passwordEncryption'),
-  require('../middlewares/user/register/register')
-);
+router.post('/register', checkUser, checkPassword1, passwordEncryption, register);
+router.post('/login', checkUser, passwordEncryption, checkPassword2, issueToken);
 
 module.exports = router;
