56037f3f4f327049d4f556cfe4a1ac6121870678
kb4ek <kbaek1202@gmail.com>
2019-07-26 14:26:31 +0900

    한세톤 앱 홍보 영상 HTML

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const sequelize = require('./database').sequelize;
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -14,10 +15,15 @@ sequelize.sync();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/category', require('./routes/controllers/category.controller'));
 app.use('/user', require('./routes/controllers/user.controller'));
 app.use('/board', require('./routes/controllers/board.controller'));
+
+app.get('/', (req, res) => {
+  return res.redirect('index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
