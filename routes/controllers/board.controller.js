6dff01d6dbf7f3c5504f104c44532ebeced2c9cb
kb4ek <kbaek1202@gmail.com>
2019-07-26 13:46:58 +0900

    /board GET /, GET /view, POST /like, POST /

--- routes/controllers/board.controller.js
+++ routes/controllers/board.controller.js
@@ -3,15 +3,25 @@ const verifyToken = require('../middlewares/jwt/verifyToken');
 
 //게시글 작성
 const postBoard = require('../middlewares/board/post/postBoard');
+//게시글 목록
+const getBoard = require('../middlewares/board/get/getBoard');
+//게시글 좋아요
+const likeBoard = require('../middlewares/board/like/likeBoard');
+//게시글 보기
+const viewBoard = require('../middlewares/board/get/viewBoard');
 
 const router = require('express').Router();
 
 router.use(verifyToken);
 
 router.post('/', postBoard);
+router.get('/', getBoard);
 
+router.get('/view', viewBoard);
+router.post('/like', likeBoard);
+/* 
 router.delete('/');
 
-router.post('/like');
+router.post('/like'); */
 
 module.exports = router;
